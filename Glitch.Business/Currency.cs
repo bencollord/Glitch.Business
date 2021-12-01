d201a808a7d96d38f8854bac74bb6c3197606aeb
bencollord <g.ben.collord@gmail.com>
2021-11-30 17:55:35 -0700

    Add files via upload

---
+++ Glitch.Business/Currency.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Glitch.Business
+{
+    [Serializable]
+    public sealed class Currency : IEquatable<Currency>, IFormatProvider, ICustomFormatter
+    {
+        private const string UnknownCurrencySymbol = "¤";
+
+        private static readonly Lazy<HashSet<Currency>> lazyCache = new Lazy<HashSet<Currency>>(LoadCurrencies);
+
+        private string name;
+        private string symbol;
+        private string isoCode;
+        private int isoNumber;
+        private int minorUnits;
+
+        private Currency(string isoCode, int isoNumber, string name, string symbol, int minorUnits)
+        {
+            Guard.NotNullOrEmpty(isoCode, nameof(isoCode));
+            Guard.NotLessThanZero(isoNumber, nameof(isoNumber));
+
+            this.name = String.IsNullOrEmpty(name) ? isoCode : name;
+            this.symbol = String.IsNullOrEmpty(symbol) ? UnknownCurrencySymbol : symbol;
+            this.isoCode = isoCode;
+            this.isoNumber = isoNumber;
+            this.minorUnits = minorUnits;
+        }
+
+        public static readonly Currency None = new Currency("XXX", 999, "No Currency", UnknownCurrencySymbol, 0);
+
+        public static Currency Current => GetInstance(CultureInfo.CurrentCulture);
+
+        private static HashSet<Currency> Cache => lazyCache.Value;
+
+        public static Currency GetInstance(int isoNumber)
+        {
+            return Cache.Where(i => i.IsoNumber == isoNumber).DefaultIfEmpty(None).Single();
+        }
+
+        public static Currency GetInstance(string isoCode)
+        {
+            return Cache.Where(i => i.IsoCode == isoCode).DefaultIfEmpty(None).Single();
+        }
+
+        public static Currency GetInstance(RegionInfo region)
+        {
+            Guard.NotNull(region, nameof(region));
+
+            var currency = Cache.SingleOrDefault(c => StringComparer.InvariantCultureIgnoreCase.Equals(c.IsoCode, region.ISOCurrencySymbol));
+
+            return currency ?? None;
+        }
+
+        public static Currency GetInstance(CultureInfo culture)
+        {
+            Guard.NotNull(culture, nameof(culture));
+            Guard.Require(!culture.IsNeutralCulture, "Cannot get currency from neutral culture"); // TODO move to Errors.resx
+
+            if (culture.Equals(CultureInfo.InvariantCulture))
+            {
+                return None;
+            }
+
+            return GetInstance(new RegionInfo(culture.LCID));
+        }
+
+        private static HashSet<Currency> LoadCurrencies()
+        {
+            var xml = XDocument.Load(@"C:\Users\bcollord\source\repos\Glitch\Glitch\Business\Currencies.xml");
+
+            return xml.Descendants("Currency")
+                .Select(x => new
+                {
+                    IsoCode = x.Element("IsoCode").Value,
+                    IsoNumber = x.Element("IsoNumber").Value.Parse<int>(),
+                    Name = x.Element("Name").Value,
+                    Symbol = x.Element("Symbol")?.Value ?? UnknownCurrencySymbol,
+                    MinorUnits = Int32.TryParse(x.Element("MinorUnits")?.Value, out int i) ? i : 0
+                })
+                .Select(x => new Currency(x.IsoCode, x.IsoNumber, x.Name, x.Symbol, x.MinorUnits))
+                .ToHashSet();
+        }
+
+        public string Name => name;
+
+        public string Symbol => symbol;
+
+        public string IsoCode => isoCode;
+
+        public int IsoNumber => isoNumber;
+
+        public int MinorUnits => minorUnits;
+
+        public bool Equals(Currency other)
+        {
+            if (ReferenceEquals(other, null)) return false;
+            if (ReferenceEquals(other, this)) return true;
+
+            return StringComparer.OrdinalIgnoreCase.Equals(IsoCode, other.IsoCode)
+                && IsoNumber == other.IsoNumber;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Currency);
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+
+            hash.Add(isoNumber);
+            hash.Add(isoCode, StringComparer.OrdinalIgnoreCase);
+
+            return hash.ToHashCode();
+        }
+
+        public override string ToString() => $"{Name} ({IsoCode})";
+
+        public string Format(string format, Money money, IFormatProvider provider)
+        {
+            var customFormatSpecifiers = new char[] { '0', '#', '.', ',' };
+            var allowedFormatSpecifiers = new char[] { 'C', 'F', 'G', 'N' };
+
+            var numberFormat = provider?.GetFormat(typeof(NumberFormatInfo)) as NumberFormatInfo;
+
+            if (numberFormat == null)
+            {
+                var culture = provider?.GetFormat(typeof(CultureInfo)) as CultureInfo ?? CultureInfo.CurrentCulture;
+
+                numberFormat = culture.NumberFormat;
+            }
+
+            decimal value = money.ToDecimal();
+
+            if (format.All(c => customFormatSpecifiers.Contains(c)))
+            {
+                var formattedValue = value.ToString(format, numberFormat);
+
+                return AppendSymbol(formattedValue, numberFormat);
+            }
+
+            int precision = 2;
+
+            if (format.Length > 1 && Int32.TryParse(format.Substring(1), out int i))
+            {
+                precision = i;
+            }
+
+            switch (format[0].ToUpperInvariant())
+            {
+                case 'L':
+                    return String.Format($"{{0:N{precision}}} {{1}}", value, IsoCode);
+                case 'I':
+                    return String.Format($"{{0:F{precision}}} {{1}}", value, IsoCode);
+                case 'C':
+                case 'F':
+                case 'G':
+                case 'N':
+                    return value.ToString(format, GetNumberFormat());
+                default:
+                    throw new FormatException(Errors.Format_Money);
+            }
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(CultureInfo))
+            {
+                var culture = CultureInfo.CurrentCulture.Clone() as CultureInfo;
+                var numberFormat = culture.NumberFormat.Clone() as NumberFormatInfo;
+
+                numberFormat.CurrencyDecimalDigits = minorUnits;
+                numberFormat.CurrencySymbol = Symbol;
+                culture.NumberFormat = numberFormat;
+
+                return culture;
+            }
+            if (formatType == typeof(NumberFormatInfo))
+            {
+                return GetNumberFormat();
+            }
+            if (formatType == typeof(Currency))
+            {
+                return this;
+            }
+
+            return null;
+        }
+
+        private NumberFormatInfo GetNumberFormat()
+        {
+            var numberFormat = CultureInfo.CurrentCulture.NumberFormat.Clone() as NumberFormatInfo;
+
+            numberFormat.CurrencySymbol = Symbol;
+            numberFormat.CurrencyDecimalDigits = minorUnits;
+
+            return numberFormat;
+        }
+
+        private string AppendSymbol(string formatted, NumberFormatInfo numberFormat)
+        {
+            switch (numberFormat.CurrencyPositivePattern)
+            {
+                case 0: return $"{Symbol}{formatted}";
+                case 1: return $"{formatted}{Symbol}";
+                case 2: return $"{Symbol} {formatted}";
+                case 3: return $"{formatted} {Symbol}";
+                default:
+                    Debug.Assert(false, "If we made it here, the .NET Framework is broken.");
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        string ICustomFormatter.Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg is Money m)
+            {
+                return Format(format, m, formatProvider);
+            }
+            if (arg is IFormattable f)
+            {
+                return f.ToString(format, CultureInfo.CurrentCulture);
+            }
+
+            return arg.ToString();
+        }
+    }
+}
---
+++ Glitch.Business/Money.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Globalization;
+
+namespace Glitch.Business
+{
+    [Serializable]
+    public struct Money : IEquatable<Money>, 
+        IComparable<Money>, IEquatable<decimal>, 
+        IComparable<decimal>, IComparable, 
+        IFormattable, IConvertible
+    {
+        private decimal value;
+        private int currencyCode;
+
+        public Money(decimal value)
+            : this(value, Currency.Current) { }
+
+        public Money(decimal value, Currency currency)
+            : this(value, currency.IsoNumber) { }
+
+        public Money(decimal value, string currencyCode)
+            : this(value, Currency.GetInstance(currencyCode)) { }
+
+        public Money(decimal value, int currencyCode)
+        {
+            this.value = value;
+            this.currencyCode = currencyCode;
+        }
+
+        public static Money Zero => new Money(0, Currency.Current);
+
+        public static Money Parse(string input) => Parse(input, Currency.Current);
+
+        public static Money Parse(string input, IFormatProvider formatProvider)
+            => Parse(input, NumberStyles.Currency, formatProvider);
+        
+        public static Money Parse(string input, NumberStyles style)
+            => Parse(input, style, Currency.Current);
+
+        public static Money Parse(string input, NumberStyles style, IFormatProvider formatProvider)
+        {
+            if (TryParse(input, style, formatProvider, out Money result))
+            {
+                return result;
+            }
+
+            throw new FormatException(Errors.Format_Money);
+        }
+
+        public static bool TryParse(string input, out Money result)
+            => TryParse(input, NumberStyles.Currency, Currency.Current, out result);
+
+        public static bool TryParse(string input, NumberStyles styles, IFormatProvider formatProvider, out Money result)
+        {
+            bool success = Decimal.TryParse(input, styles, formatProvider, out decimal value);
+
+            if (!success)
+            {
+                result = default;
+                return false;
+            }
+
+            var culture = formatProvider.GetFormat(typeof(CultureInfo)) as CultureInfo;
+            var currency = Currency.GetInstance(culture ?? CultureInfo.CurrentCulture);
+
+            result = new Money(value, currency);
+            return true;
+        }
+
+        public Money Add(Money other)
+        {
+            Guard.Require(currencyCode == other.currencyCode, Errors.InvalidCurrency);
+
+            return new Money(value + other.value, currencyCode);
+        }
+
+        public Money Add(decimal value) => new Money(this.value + value, currencyCode);
+
+        public Money Subtract(Money other)
+        {
+            Guard.Require(currencyCode == other.currencyCode, Errors.InvalidCurrency);
+
+            return new Money(value - other.value, currencyCode);
+        }
+
+        public Money Subtract(decimal value) => new Money(this.value - value, currencyCode);
+
+        public Money Multiply(Money other)
+        {
+            Guard.Require(currencyCode == other.currencyCode, Errors.InvalidCurrency);
+
+            return new Money(value * other.value, currencyCode);
+        }
+
+        public Money Multiply(decimal value) => new Money(this.value * value, currencyCode);
+
+        public Money Divide(Money other)
+        {
+            Guard.Require(currencyCode == other.currencyCode, Errors.InvalidCurrency);
+
+            return new Money(value / other.value, currencyCode);
+        }
+
+        public Money Divide(decimal value) => new Money(this.value / value, currencyCode);
+
+        public Money Remainder(Money other)
+        {
+            Guard.Require(currencyCode == other.currencyCode, Errors.InvalidCurrency);
+
+            return new Money(value % other.value, currencyCode);
+        }
+
+        public Money Remainder(decimal value) => new Money(this.value % value, currencyCode);
+
+        public Money Round()
+            => new Money(Decimal.Round(value), currencyCode);
+
+        public Money Round(int places)
+            => new Money(Decimal.Round(value, places), currencyCode);
+
+        public Money Round(MidpointRounding rounding)
+            => new Money(Decimal.Round(value, rounding), currencyCode);
+
+        public Money Round(int places, MidpointRounding rounding) 
+            => new Money(Decimal.Round(value, places, rounding), currencyCode);
+
+        public Money Floor() => new Money(Decimal.Floor(value), currencyCode);
+
+        public Money Ceiling() => new Money(Decimal.Ceiling(value), currencyCode);
+
+        public Money Negate() => new Money(Decimal.Negate(value), currencyCode);
+
+        public Money Abs() => new Money(Math.Abs(value), currencyCode);
+
+        public Money ChangeCurrency(Currency currency, decimal exchangeRate)
+            => new Money(value * exchangeRate, currency);
+
+        public Currency GetCurrency() => Currency.GetInstance(currencyCode);
+
+        public int CompareTo(Money other)
+        {
+            if (currencyCode != other.currencyCode)
+            {
+                return currencyCode.CompareTo(other.currencyCode);
+            }
+
+            return value.CompareTo(other.value);
+        }
+
+        public int CompareTo(decimal value) => this.value.CompareTo(value);
+
+        public int CompareTo(object obj)
+        {
+            switch (obj)
+            {
+                case Money m:
+                    return CompareTo(m);
+                case decimal d:
+                    return CompareTo(d);
+                default:
+                    throw new ArgumentException(Errors.Argument_MustBeMoney);
+            }
+        }
+
+        public bool Equals(Money other)
+        {
+            return value == other.value
+                && currencyCode == other.currencyCode;
+        }
+
+        public bool Equals(decimal value) => this.value.Equals(value);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this)) return true;
+
+            if (obj is Money)
+            {
+                return Equals((Money)obj);
+;           }
+            if (obj is decimal)
+            {
+                return Equals((decimal)obj);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(value, currencyCode);
+
+        public override string ToString() => ToString("C");
+
+        public string ToString(string format) => ToString(format, null);
+
+        public string ToString(IFormatProvider formatProvider) => ToString("C", formatProvider);
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            format = format.ToUpperInvariant();
+
+            if (format.StartsWith("G"))
+            {
+                format = format.Replace("G", "C");
+            }
+            if (formatProvider == null)
+            {
+                formatProvider = GetCurrency();
+            }
+
+            var formatter = formatProvider.GetFormat(typeof(ICustomFormatter));
+
+            if (formatter is ICustomFormatter customFormatter)
+            {
+                return customFormatter.Format(format, this, formatProvider);
+            }
+
+            formatter = formatProvider.GetFormat(typeof(Currency));
+
+            if (formatter is Currency c)
+            {
+                return c.Format(format, this, formatProvider);
+            }
+
+            formatter = formatProvider.GetFormat(typeof(NumberFormatInfo));
+
+            if (formatter is NumberFormatInfo numberFormat)
+            {
+                return value.ToString(format, numberFormat);
+            }
+
+            return value.ToString(format, NumberFormatInfo.CurrentInfo);
+        }
+
+        public TypeCode GetTypeCode() => TypeCode.Decimal;
+
+        public int ToInt32() => (int)value;
+
+        public long ToInt64() => (long)value;
+
+        public decimal ToDecimal() => value;
+
+        public static bool operator ==(Money x, Money y) => x.Equals(y);
+        public static bool operator !=(Money x, Money y) => !x.Equals(y);
+        public static bool operator >(Money x, Money y) => x.CompareTo(y) == 1;
+        public static bool operator <(Money x, Money y) => x.CompareTo(y) == -1;
+        public static bool operator >=(Money x, Money y) => x.CompareTo(y) >= 0;
+        public static bool operator <=(Money x, Money y) => x.CompareTo(y) <= 0;
+
+        public static Money operator +(Money x, Money y) => x.Add(y);
+        public static Money operator -(Money x, Money y) => x.Subtract(y);
+        public static Money operator *(Money x, Money y) => x.Multiply(y);
+        public static Money operator /(Money x, Money y) => x.Divide(y);
+        public static Money operator %(Money x, Money y) => x.Remainder(y);
+
+        public static Money operator -(Money x) => x.Negate();
+        public static Money operator ++(Money x) => new Money(++x.value, x.currencyCode);
+        public static Money operator --(Money x) => new Money(--x.value, x.currencyCode);
+
+        public static explicit operator Money(decimal x) => new Money(x);
+        public static implicit operator decimal(Money x) => x.value;
+
+        public static explicit operator int(Money x) => (int)x.value;
+        public static explicit operator short(Money x) => (short)x.value;
+        public static explicit operator long(Money x) => (long)x.value;
+        public static explicit operator uint(Money x) => (uint)x.value;
+        public static explicit operator ushort(Money x) => (ushort)x.value;
+        public static explicit operator ulong(Money x) => (ulong)x.value;
+        public static explicit operator byte(Money x) => (byte)x.value;
+        public static explicit operator sbyte(Money x) => (sbyte)x.value;
+        public static explicit operator float(Money x) => (float)x.value;
+        public static explicit operator double(Money x) => (double)x.value;
+
+        public static explicit operator Money(int x) => new Money(x);
+        public static explicit operator Money(short x) => new Money(x);
+        public static explicit operator Money(long x) => new Money(x);
+        public static explicit operator Money(uint x) => new Money(x);
+        public static explicit operator Money(ushort x) => new Money(x);
+        public static explicit operator Money(ulong x) => new Money(x);
+        public static explicit operator Money(byte x) => new Money(x);
+        public static explicit operator Money(sbyte x) => new Money(x);
+        public static explicit operator Money(float x) => new Money((decimal)x);
+        public static explicit operator Money(double x) => new Money((decimal)x);
+
+        public static Money operator +(Money x, decimal y) => x.Add(y);
+        public static Money operator -(Money x, decimal y) => x.Subtract(y);
+        public static Money operator *(Money x, decimal y) => x.Multiply(y);
+        public static Money operator /(Money x, decimal y) => x.Divide(y);
+        public static Money operator %(Money x, decimal y) => x.Remainder(y);
+
+        public static bool operator ==(Money x, decimal y) => x.Equals(y);
+        public static bool operator !=(Money x, decimal y) => !x.Equals(y);
+        public static bool operator >(Money x, decimal y) => x.CompareTo(y) == 1;
+        public static bool operator <(Money x, decimal y) => x.CompareTo(y) == -1;
+        public static bool operator >=(Money x, decimal y) => x.CompareTo(y) >= 0;
+        public static bool operator <=(Money x, decimal y) => x.CompareTo(y) <= 0;
+
+        bool IConvertible.ToBoolean(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToBoolean(provider);
+
+        char IConvertible.ToChar(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToChar(provider);
+
+        sbyte IConvertible.ToSByte(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToSByte(provider);
+
+        byte IConvertible.ToByte(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToByte(provider);
+
+        short IConvertible.ToInt16(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToInt16(provider);
+
+        ushort IConvertible.ToUInt16(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToUInt16(provider);
+
+        int IConvertible.ToInt32(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToInt32(provider);
+
+        uint IConvertible.ToUInt32(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToUInt32(provider);
+
+        long IConvertible.ToInt64(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToInt64(provider);
+
+        ulong IConvertible.ToUInt64(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToUInt64(provider);
+
+        float IConvertible.ToSingle(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToSingle(provider);
+
+        double IConvertible.ToDouble(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToDouble(provider);
+
+        decimal IConvertible.ToDecimal(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToDecimal(provider);
+
+        DateTime IConvertible.ToDateTime(IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToDateTime(provider);
+
+        object IConvertible.ToType(Type conversionType, IFormatProvider provider)
+            => value.CastAs<IConvertible>().ToType(conversionType, provider);
+    }
+}
